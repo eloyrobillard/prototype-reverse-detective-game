1741271043d0dddad7f0b31aed91b646a068a835
Eloy Robillard <eloy.robillard@gmail.com>
2026-03-07 19:51:15 +0100

    Add Alucard sitting on the mud man

---
+++ scripts/alucard.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	animated_sprite_2d.play("sitting")
