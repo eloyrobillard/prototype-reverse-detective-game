cb66e85f154c224d821d68cd9d77f0785d73fee2
Eloy Robillard <eloy.robillard@gmail.com>
2026-03-12 21:16:12 +0100

    Add dialogue animation and reaction to input

--- scripts/dialogue_cursor.gd
+++ scripts/dialogue_cursor.gd
@@ -1,13 +1,26 @@
 extends TextureRect
 
+var tween: Tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var tween = get_tree().create_tween().bind_node(self).set_loops()
+	visible = false
+	
+	tween = get_tree().create_tween().bind_node(self).set_loops()
 	tween.tween_property(self, "visible", false, 0.15)
 	tween.tween_property(self, "visible", true, 0.15)
+	tween.pause()
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func set_active(flag: bool) -> void:
+	if flag:
+		visible = true
+		tween.play()
+	else:
+		tween.pause()
+		visible = false
