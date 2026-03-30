6dd27c852c3b17dd466099979ad2a0614d17361f
Eloy Robillard <eloy.robillard@gmail.com>
2026-03-30 20:54:22 +0200

    Remove refs to Alucard's visibility notifier

--- scripts/alucard.gd
+++ scripts/alucard.gd
@@ -1,9 +1,6 @@
 extends CharacterBody2D
 
-signal on_screen
-
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 
 # NOTE: それぞれのアニメーションの向きはなぜか違う
 # sitting -> 左を向いている
@@ -18,7 +15,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_dialogue_layer_alucard_dialogue_ended() -> void:
-	visible_on_screen_notifier_2d.free()
