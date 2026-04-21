8fff634a55359a68b6a12e9e1711f825c47024fc
Eloy Robillard <eloy.robillard@gmail.com>
2026-04-21 21:27:08 +0200

    Switch to chamber on player enter

--- scripts/exit_trigger.gd
+++ scripts/exit_trigger.gd
@@ -6,16 +6,6 @@ signal player_entered
 signal player_left
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_alucard_left_scene() -> void:
 	collision_shape_2d.disabled = false
 
@@ -23,7 +13,7 @@ func _on_alucard_left_scene() -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player_entered.emit()
-		get_tree().change_scene_to_file("res://scenes/???.tscn")
+		get_tree().change_scene_to_file("res://scenes/chamber.tscn")
 
 
 func _on_body_exited(body: Node2D) -> void:
